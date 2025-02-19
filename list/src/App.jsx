ee30626b4173ae0d1e915ed101be5c25089198da
rhuan-rgb <rhuan.lima.cmd@gmail.com>
2025-02-19 11:01:38 -0300

    lista de usuarios

---
+++ list/src/App.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react'
+// Imports para criação de tabela
+import Table from '@mui/material/Table';
+import TableContainer from '@mui/material/TableContainer';
+
+// TableHead é onde colocamos os titulos
+import TableHead from '@mui/material/TableHead';
+
+// TableBody é onde colocamos o conteúdo
+import TableBody from '@mui/material/TableBody';
+
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import Paper from '@mui/material/Paper';
+import api from './axios/axios';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  async function getUsers(){
+    //chamada da api
+    await api.getUsers().then(
+      (response)=>{
+        console.log(response.data.users);
+        setUsers(response.data.users);
+      },(error)=>{
+        console.log("Erro:", error)
+      }
+    )
+  }
+
+  const listUsers = users.map((user)=>{
+    return(
+      <TableRow key={user.id_usuario}>
+        <TableCell align="center">{user.name}</TableCell>
+        <TableCell align="center">{user.email}</TableCell>
+        <TableCell align="center">{user.cpf}</TableCell>
+      </TableRow>
+    )
+  })
+
+  useEffect(()=>{
+    //Aqui deve-se criar ou chamar uma função
+    getUsers();
+
+  },[]);
+  
+  return (
+    <div>
+        <h5>Lista de usuários</h5>
+        <TableContainer component={Paper} style={{margin:"2px"}}>
+          <Table size="small">
+            <TableHead style={{backgroundColor: "brown", borderStyle: "solid"}}>
+              <TableRow>
+                <TableCell align="center">
+                  Nome
+                </TableCell>
+                <TableCell align="center">
+                  Email
+                </TableCell>
+                <TableCell align="center">
+                  CPF
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>{listUsers}</TableBody>
+          </Table>
+        </TableContainer>
+    </div>
+  )
+}
+
+export default App
